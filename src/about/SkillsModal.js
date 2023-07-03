5a747eda285b6bfb722a091bdb99245ab519c7e7
Garvit Jain <garvitjain09@gmail.com>
2023-07-03 18:23:35 +0000

    Updated Responsive to accomdate mobile view

--- src/about/SkillsModal.js
+++ src/about/SkillsModal.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useAppSelector, getUsersData } from "../store";
-
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 export default function BasicModal({ open, setOpen, skill }) {
@@ -30,7 +27,7 @@ export default function BasicModal({ open, setOpen, skill }) {
       >
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            {skill.name}
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
