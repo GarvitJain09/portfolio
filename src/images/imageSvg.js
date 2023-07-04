bb32031f0228f46757682a2e0d5a523ba267cf99
Garvit Jain <garvitjain09@gmail.com>
2023-07-04 09:12:05 +0000

    Update imageSvg.js and styles.css

--- src/images/imageSvg.js
+++ src/images/imageSvg.js
@@ -32,7 +32,7 @@ const CodeSandBox = () => {
     <svg
       viewBox="0 0 16 16"
       xmlns="http://www.w3.org/2000/svg"
-      className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root codeSandBox"
+      className="CodeSandBoxClass"
       style={{ fill: "transparent" }}
     >
       <path
